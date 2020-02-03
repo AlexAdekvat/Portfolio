77a415b126a5308c213326e67a5716c7b1645916
AlexAdekvat <morozov.lesha.1998@mail.ru>
2020-02-04 01:24:57 +0300

    Updates

---
+++ precache-manifest.61d1282f20e2763cba4746104d626079.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ef4eb38489c2db31fc6813937aedcb4",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "2dc5e1aab2f531577ff7",
+    "url": "/Portfolio/static/css/main.b2c07830.chunk.css"
+  },
+  {
+    "revision": "cc170d3f2a3827ac672b",
+    "url": "/Portfolio/static/js/2.ba12357e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Portfolio/static/js/2.ba12357e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2dc5e1aab2f531577ff7",
+    "url": "/Portfolio/static/js/main.db46e32b.chunk.js"
+  },
+  {
+    "revision": "38e9353468597077cd3e",
+    "url": "/Portfolio/static/js/runtime-main.ed271e06.js"
+  },
+  {
+    "revision": "9cab3941a9641e93c85fb1b45f2da6a2",
+    "url": "/Portfolio/static/media/Github.9cab3941.svg"
+  },
+  {
+    "revision": "84b4eb257e0a43a74a14e51d17d8794e",
+    "url": "/Portfolio/static/media/Telegram.84b4eb25.svg"
+  },
+  {
+    "revision": "21b808eca0c7c1ba4913c687ecc6c302",
+    "url": "/Portfolio/static/media/Twitter.21b808ec.svg"
+  },
+  {
+    "revision": "258a9e69a7fed9f26c79a912d3c113c1",
+    "url": "/Portfolio/static/media/vk.258a9e69.svg"
+  }
+]);
